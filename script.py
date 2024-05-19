6aaa7bb54aef1c175947e39cd359d941c6dea84e
Ivan E. Cao-Berg <icaoberg@cmu.edu>
2024-05-19 01:19:45 -0400

    Updated script

--- script.py
+++ script.py
@@ -1,35 +1,18 @@
 #!/usr/bin/env python
-# coding: utf-8
-
-# In[5]:
-
-
+import streamlit as st
 from great_tables import GT
 import pandas as pd
 from pprint import pprint
 from datetime import datetime
 
+@st.cache_data
+def get_data():
+    f = 'https://download.brainimagelibrary.org/inventory/daily/reports/today.json'
+    df = pd.read_json(f)
+    df = df.drop(columns=['bildirectory', 'bildate', 'genotype', 'md5_coverage', 'sha256_coverage', 'xxh64_coverage', 'taxonomy', 'size', 'samplelocalid', 'json_file', 'temp_file', 'creation_date', 'exists', 'dataset_uuid'])
+    return df
 
-# In[6]:
-
-
-f = 'https://download.brainimagelibrary.org/inventory/daily/reports/today.json'
-df = pd.read_json(f)
-df = df.drop(columns=['bildirectory', 'bildate', 'genotype', 'md5_coverage', 'sha256_coverage', 'xxh64_coverage', 'taxonomy', 'size', 'samplelocalid', 'json_file', 'temp_file', 'creation_date', 'exists', 'dataset_uuid'])
-
-def create_bar(prop_fill: float, max_width: int, height: int) -> str:
-    """Create divs to represent prop_fill as a bar."""
-    width = round(max_width * prop_fill, 2)
-    px_width = f"{width}px"
-    return f"""\
-    <div style="width: {max_width}px; background-color: lightgrey;">\
-        <div style="height:{height}px;width:{px_width};background-color:green;"></div>\
-    </div>\
-    """
-
-
-# In[8]:
-
+df = get_data()
 
 # Create a display table
 (
@@ -61,16 +44,6 @@ def create_bar(prop_fill: float, max_width: int, height: int) -> str:
     )
     .tab_source_note(source_note="The Brain Image Library is supported by the National Institutes of Mental Health of the National Institutes of Health under award number R24-MH-114793. The content is solely the responsibility of the authors and does not necessarily represent the official views of the National Institutes of Health.")
 )
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
 
 
 
