bb1e18c62f9f93565a365b2b7b9bd07bb6257474
Ivan E. Cao-Berg <icaoberg@cmu.edu>
2024-05-19 14:12:24 -0400

    Modified app to display unfinished datasets.

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 import streamlit as st
 from great_tables import GT
 import pandas as pd
@@ -10,10 +9,11 @@ st.write("# Daily Inventory Status")
 
 @st.cache_data
 def get_data():
-    st.write('Downloading data from Brain Image Library')
     f = 'https://download.brainimagelibrary.org/inventory/daily/reports/today.json'
     df = pd.read_json(f)
-    df = df.drop(columns=['bildirectory', 'consortium', 'bildate', 'genotype', 'md5_coverage', 'sha256_coverage', 'xxh64_coverage', 'taxonomy', 'size', 'samplelocalid', 'json_file', 'temp_file', 'creation_date', 'exists', 'dataset_uuid'])
+    df = df.drop(columns=['consortium', 'bildate', 'genotype', 'md5_coverage', 'sha256_coverage', 'xxh64_coverage', 'taxonomy', 'size', 'samplelocalid', 'json_file', 'temp_file', 'creation_date', 'exists', 'dataset_uuid'])
+    df = df[df['score'] < 1]
+    df = df[['score'] + [col for col in df.columns if col != 'score']]
     df = df.sort_values(by=['number_of_files'])
     df.rename(columns = {'metadata_version':"Version",
         'bildid':"ID",
@@ -23,6 +23,7 @@ def get_data():
         'project':"Project",
         'generalmodality':'Modality',
         'technique':'Technique',
+	'bildirectory':'Location',
         'species':'Species',
         'pretty_size':'Size',
         'number_of_files':'Number of Files',
@@ -31,6 +32,7 @@ def get_data():
     return df
 
 df = get_data()
+st.write(f'Number of public datasets left to process is {len(df)}.')
 st.data_editor(
     df,
     column_config={
